ce21edaebe2e217ae7dba7b8c3168acf9ade7eb3
prabeen6260 <prabingiri922@gmail.com>
2025-10-25 22:10:01 -0500

    a

--- frontend/src/container/navbar/navbar.jsx
+++ frontend/src/container/navbar/navbar.jsx
@@ -18,7 +18,7 @@ export default function Navbar() {
     const handleLogout = () => {
         logout({
             logoutParams: {
-                returnTo: window.location.origin,
+                returnTo: meta.env.VITE_AUTH0_LOGOUT_REDIRECT_URI ||window.location.origin,
             },
         })
     }
