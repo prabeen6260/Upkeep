832741854ef32f0d7afd91f1e94a4311f789cb59
prabeen6260 <prabingiri922@gmail.com>
2025-10-25 21:57:05 -0500

    fixng auth0

--- frontend/src/config/auth0.js
+++ frontend/src/config/auth0.js
@@ -7,7 +7,8 @@ export const auth0Config = {
     audience: import.meta.env.VITE_AUTH0_AUDIENCE,
   },
   logoutParams: {
-    returnTo: import.meta.env.VITE_AUTH0_LOGOUT_REDIRECT_URI || window.location.origin,
+    //returnTo: import.meta.env.VITE_AUTH0_LOGOUT_REDIRECT_URI || window.location.origin,
+    returnTo: "https://prabeen6260.github.io/Upkeep/",
     client_id: import.meta.env.VITE_AUTH0_CLIENT_ID,
   },
   useRefreshTokens: true,
